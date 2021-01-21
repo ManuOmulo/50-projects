4602991fe8034bf8244d3db15098754a11946b2c
ManuOmulo <omuloemmanuel@gmail.com>
2021-01-21 09:07:09 +0300

    Dad Jokes

---
+++ 10_Dad_Jokes/script.js
@@ -0,0 +1,18 @@
+const jokeElement = document.getElementById("joke")
+const jokeBtn = document.getElementById("joke-btn")
+
+const generateJoke = async () => {
+  const config = {
+    headers: {
+      "Accept": "application/json;"
+    }
+  }
+
+  const res = await fetch("https://icanhazdadjoke.com", config)
+  const data = await res.json()
+  jokeElement.innerHTML = data.joke
+}
+
+generateJoke()
+
+jokeBtn.addEventListener("click", generateJoke)
